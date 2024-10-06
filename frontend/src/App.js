c1b57d74ca2b02865fbba7666d9a77d21f6dd5c1
Martina Laventure <martina.laventure@correo.ucu.edu.uy>
2024-10-06 20:20:22 -0300

    manejo de rutas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,54 @@
+import React, { useState } from 'react';
 import UserSignUp from './componentes/userSignUp';
+import LogIn from './componentes/logIn'; //cambiar dependiendo del nombre que le pongan
+//import Feed from './componentes/feed'; //cambiar dependiendo del nombre que le pongan
+//import Perfil from './componentes/perfil'; //cambiar dependiendo del nombre que le pongan
+//import PerfilAmigo from './componentes/perfilAmigo'; //cambiar dependiendo del nombre que le pongan
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import './App.css';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true); 
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false); 
+  };
+
   return (
     <div className="App">
-      <UserSignUp />
+      <Router>
+        <Routes>
+          
+          {/* Ruta de login */}
+        
+          <Route path="/login" element={<LogIn onLogin={handleLogin} />} />
+          
+
+          {/* Ruta de signup */}
+          <Route path="/signup" element={<UserSignUp />} />
+
+          {/* Ruta privada para el feed */}
+          {/*
+          <Route
+            path="/feed"
+            element={isLoggedIn ? <Feed /> : <Navigate to="/login" />}
+          />
+          */}
+
+          {/* Ruta privada para el perfil */}
+          {/*
+          <Route
+            path="/perfil"
+            element={isLoggedIn ? <Perfil onLogout={handleLogout} /> : <Navigate to="/login" />}
+          />
+          */}
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/componentes/logIn.jsx
@@ -0,0 +1,8 @@
+const LogIn = () => {
+    return (
+        <h1>Log in usuario</h1>
+    );
+
+}
+
+export default LogIn;
