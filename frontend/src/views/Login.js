90d39250dc61a1b24b1e2e388896516c7c3ff3f2
camiloDanielli <daniellicamilo6@gmail.com>
2024-10-07 15:53:29 -0300

    Componente y vista del loguin v1 utilizando post, sin cambio booleando para habilitar paginas

---
+++ frontend/src/views/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserLogin from '../components/UserLogin';
+
+const Login = () => {
+    return (
+        <div className="login-view">
+            <h2>Login Page</h2>
+            <UserLogin />
+        </div>
+    );
+};
+
+export default Login;
