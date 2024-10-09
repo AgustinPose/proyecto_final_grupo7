7a7d3f00b65668c994cf874fb2a68e60d2b08336
DiegoMorenoArias <diego.morenoa@correo.ucu.edu.uy>
2024-10-09 17:36:10 -0300

    Agregó feed

--- frontend/src/views/feed.js
+++ frontend/src/views/feed.js
@@ -1,10 +1,28 @@
 import React from 'react';
-
-const Feed = ({ onLogout }) => {
+import '../Feed.css';
+import Img from "../images/playa.jpeg"
+import Matecito from "../images/mate.png"
+const Feed = () => {
     return (
-        <div>
-            <h1>Bienvenido al Feed</h1>
-            <button onClick={onLogout}>Cerrar sesión</button>
+        <div className="feed-view">
+            <header className="feed-header">
+                <h1>R</h1>
+                <p>Donde las conexiones florecen...</p> {/* Nuestro eslogan */}
+            </header>
+            
+            <div className="feed-posts">
+                <div className="post-card">
+                    <h3>UsuariazOwO</h3>
+                    <p>En casita, resfriado</p>
+                    <img src={Matecito} alt="Mateando" />
+                </div>
+                <div className="post-card">
+                    <h3>superUsuarito2004</h3>
+                    <p>Vacacionando en la playa re tranquiiii B)</p>
+                    <img src={Img} alt="playa" />
+                </div>
+                {/* Etcétera */}
+            </div>
         </div>
     );
 };
