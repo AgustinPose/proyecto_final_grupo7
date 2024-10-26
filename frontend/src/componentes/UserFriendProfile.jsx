0f5da96d109bc9d70772c3efa6be80b183393f23
AgustinPose <poseagus15@gmail.com>
2024-10-26 17:07:37 -0300

    añado amigos

--- frontend/src/componentes/UserFriendProfile.jsx
+++ frontend/src/componentes/UserFriendProfile.jsx
@@ -1,102 +1,174 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import Sidebar from '../views/sidebar';
-import PerfilDefecto from "../images/perfilDefecto.jpg";
 import "../css/UserFriendProfile.css";
+import PerfilDefecto from '../images/perfilDefecto.jpg';
 
-const UserFriendProfile = ({ friendId }) => {
+const UserProfileFriend = () => {
+  const { friendId } = useParams();
   const [friendData, setFriendData] = useState(null);
-  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
   const [isFriend, setIsFriend] = useState(false);
-  const [friendsCount, setFriendsCount] = useState(0);
+  const [feedPosts, setFeedPosts] = useState([]);
+  const currentUserId = localStorage.getItem("_id");
 
-  useEffect(() => {
+  const loadFriendData = async () => {
     const token = localStorage.getItem('token');
-
-    if (!token) {
-      setError('Usuario no autenticado. Por favor, inicie sesión.');
-      return;
-    }
-
-    const fetchFriendProfile = async () => {
-      try {
-        const response = await fetch(`http://localhost:3001/api/user/profile/${friendId}`, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}` 
-          }
-        });
-
-        if (!response.ok) {
-          throw new Error('Error al obtener los datos del perfil del amigo');
-        }
-
-        const data = await response.json();
-        setFriendData(data.user);
-        setFriendsCount(data.user.friends.length);
-        setIsFriend(data.user.friends.includes(localStorage.getItem('_id')));
-      } catch (error) {
-        setError(error.message);
-      }
-    };
-
-    fetchFriendProfile();
-  }, [friendId]);
-
-  const addFriend = async () => {
-    const token = localStorage.getItem('token');
-
-    if (!token) {
-      setError('Usuario no autenticado. Por favor, inicie sesión.');
-      return;
-    }
-
     try {
-      const response = await fetch(`http://localhost:3001/api/user/add-friend/${friendId}`, {
-        method: 'POST',
+      const response = await fetch(`http://localhost:3001/api/user/profile/${friendId}`, {
+        method: 'GET',
         headers: {
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
         }
       });
 
       if (!response.ok) {
-        throw new Error('Error al añadir amigo');
+        throw new Error('Error al obtener los datos del perfil');
       }
 
-      setIsFriend(true);
-      setFriendsCount(prevCount => prevCount + 1);
+      const data = await response.json();
+      setFriendData(data.user);
+      setFeedPosts(data.user.posts || []);
+
     } catch (error) {
       setError(error.message);
     }
   };
 
-  if (error) {
-    return <p>{error}</p>;
-  }
+  useEffect(() => {
+    const token = localStorage.getItem('token');
 
-  if (!friendData) {
-    return <p>Cargando perfil del amigo...</p>;
-  }
+    if (!friendId || !token) {
+      setError('No se pudo obtener la información del usuario.');
+      return;
+    }
+
+    const fetchProfile = async () => {
+      try {
+        const currentUserResponse = await fetch(`http://localhost:3001/api/user/profile/${currentUserId}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          }
+        });
+
+        if (!currentUserResponse.ok) {
+          throw new Error('Error al obtener los datos del usuario actual');
+        }
+
+        const currentUserData = await currentUserResponse.json();
+        const response = await fetch(`http://localhost:3001/api/user/profile/${friendId}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          }
+        });
+
+        if (!response.ok) {
+          throw new Error('Error al obtener los datos del perfil');
+        }
+
+        const data = await response.json();
+        setFriendData({
+          ...data.user,
+          friendsCount: data.user.friends.length
+        });
+        setFeedPosts(data.user.posts || []);
+
+        const friendsList = currentUserData.user.friends || [];
+        setIsFriend(friendsList.some(friend => friend._id === friendId));
+
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchProfile();
+  }, [friendId, currentUserId]);
+
+  const handleAddFriend = async () => {
+    const token = localStorage.getItem('token');
+
+    try {
+      const response = await fetch(`http://localhost:3001/api/user/add-friend/${friendId}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message);
+      }
+
+      setIsFriend(true);
+      await loadFriendData();
+
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  if (isLoading) return <div>Cargando...</div>;
+  if (error) return <div>Error: {error}</div>;
 
   return (
-    <div>
+    <div className='profile-friend-layout'>
       <Sidebar />
-      <h1>Perfil de Amigo</h1>
-      <img 
-        src={friendData.profilePicture || PerfilDefecto} 
-        alt="Imagen de perfil del amigo" 
-        className="profile-image" 
-      />
-      <p>ID del amigo: {friendId}</p>
-      <p>Nombre: {friendData.username}</p>
-      <p>Descripción: {friendData.description || 'No hay descripción aún.'}</p>
-      <p>Amigos: {friendsCount}</p>
-      <button onClick={addFriend} style={{ backgroundColor: isFriend ? 'blue' : 'gray' }}>
-        {isFriend ? 'Amigos' : 'Añadir Amigo'}
-      </button>
+      <div className="profile-friend-container">
+        <div className="profile-friend-header">
+          <img
+            src={friendData?.profilePicture || PerfilDefecto}
+            alt="Imagen de perfil"
+            className="profile-friend-image"
+          />
+          <div className="profile-friend-info">
+            <h1 className='username'>{friendData?.username}</h1>
+            <div className="profile-friend-stats">
+              <span>{feedPosts.length} posts</span>
+              <span>{friendData?.friends?.length || 0} friends</span>
+            </div>
+            <p className="profile-friend-description">
+              {friendData?.description || 'No hay descripción aún.'}
+            </p>
+          </div>
+          <button
+            className={`follow-friend-button ${isFriend ? 'friend-button' : ''}`}
+            onClick={handleAddFriend}
+            disabled={isFriend}
+          >
+            {isFriend ? "Amigos" : "Añadir amigo"}
+          </button>
+        </div>
+        <div className="feed-friend-container">
+          {feedPosts.length > 0 ? (
+            <div>
+              <h2>Feed de Publicaciones</h2>
+              <div className="feed-friend-grid">
+                {feedPosts.map(post => (
+                  <div key={post._id} className="feed-friend-item">
+                    <img src={post.imageUrl} alt={post.description} className="feed-friend-image" />
+                    <p>{post.description}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ) : (
+            <p>No hay publicaciones en el feed.</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default UserFriendProfile;
+export default UserProfileFriend;
